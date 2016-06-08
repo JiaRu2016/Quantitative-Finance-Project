24e617f336229fc9747f97f9661c18b6bc2a0af4
JIA Ru <jiaru2014@126.com>
2016-06-08 21:52:15 +0800

    Add files via upload

--- Analysis_v1.2.R
+++ Analysis_v1.2.R
@@ -19,19 +19,6 @@ require(assertthat)  # a tool for Defensive Programming
 require(stringr)  # 文字处理
 
 
-
-# 注：
-# 文件夹“csvStockFiles“里面是合并好的股票数据csv文件。
-# 每个股票只有一个文件，是我手工合并的（excel里复制粘贴），再另存为csv格式
-# 暂时只做了了一个股票，作为测试，后续可以添加更多。。
-
-# 0. 列出数据集中所有的股票代码 ========================================
-stock_list <- 
-  list.files("csvStockFiles") %>% 
-  str_replace(pattern = "\\.csv", replacement = "")
-cat("以下是数据集中所有可分析的股票代码：", stocks_list, sep = "\n")
-
-
 # 1. 读文件 ===========================================
 
 # 函数ReadStockFile() 
@@ -73,9 +60,7 @@ ReadStockFile <- function(stock_num)
   return(dt)
 }
 
-# 测试：
-a <- ReadStockFile("000001SH")
-b <- ReadStockFile("00001SH")
+
 
 # 2. 检查数据是否有异常  ======================================
 # 函数CheckStockData()
@@ -117,14 +102,85 @@ CheckStockData <- function(dt)
   return(dt)
 }
 
-# 测试：
-c <- CheckStockData(a)
+
 
 
 # 3. 分析  ======================================
+# 函数AnalysisDis()分析样本内/外收益率or成交量的分布
+# Argus：
+#   dt: 清理好的股票数据。
+#   what: "return" or "MinTq"
+#   when = c(in_start, in_end, out_start, out_end): 样本内/外起止点。
+#      写成可直接转化成日期格式的形式：“2015-01-05”
+#      长度可以是3或4
 
-
-AnalysisDis <- function(dt, in_start, in_end, out_start, out_end)
+AnalysisDis <- function(dt, what = "return", 
+    when = c("2015-01-05", "2015-12-31", "2016-01-04", "2016-01-29"))
 {
+  # 检查参数 --------------------------
+  when <- as.Date(when)
+  assert_that(
+    what %in% c("return", "MinTq") && length(what) == 1,
+    length(when) == 4,
+    when[1] < when[2] && when[2] <= when[3] && when[3] < when[4]
+  )
+  # 取样本内/外数据 ------------
+  # 这里对于成交量(all >= 0) 取对数
+  # 对于retrun, 先取绝对值再取对数。
+  # 
+  dt_in <- 
+    dt[TDate >= when[1] & TDate <= when[2], what, with = FALSE] %>% 
+    unlist(use.names = FALSE) %>%
+    abs() %>%
+    log()
+  dt_out <- 
+    dt[TDate >= when[3] & TDate <= when[4], what, with = FALSE] %>% 
+    unlist(use.names = FALSE) %>%
+    abs() %>%
+    log()
   
+  dt_in_g <<- dt_in  # just for debugging
+  dt_out_g <<- dt_out
+  
+  # 画图 ---------------------
+  # 这里只用R中的基础绘图系统 boxplot()
+  # 暂时先不用histogram,因为根据不同的数据要调整坐标轴，很难写成函数
+  
+  boxplot(dt_in, dt_out, 
+          xlab = "in sample         out of sample",
+          main = sprintf("distribution of log(abs( %s )) of stock %s", what, stock_num),
+          outline = FALSE)
+  
+  return(0)
 }
+
+# 4. 测试 ===================================================
+
+# 注：
+# 文件夹“csvStockFiles“里面是合并好的股票数据csv文件。
+# 每个股票只有一个文件，是我手工合并的（excel里复制粘贴），再另存为csv格式
+# 暂时只做了了一个股票，作为测试，后续可以添加更多。。
+
+# 列出数据集中所有的股票代码 
+stock_list <- 
+  list.files("csvStockFiles") %>% 
+  str_replace(pattern = "\\.csv", replacement = "")
+cat("以下是数据集中所有可分析的股票代码：", stock_list, sep = "\n")
+
+# 以"000001SH"为例，测试
+stock_num <- "000001SH"
+dt <- ReadStockFile(stock_num)
+dt <- CheckStockData(dt)
+
+AnalysisDis(dt, what = "return", 
+            when = c("2014-01-01", "2016-05-01", "2016-05-02", "2016-05-31"))
+AnalysisDis(dt, what = "MinTq",
+            when = c("2014-01-01", "2016-05-01", "2016-05-02", "2016-05-31"))
+
+
+
+
+
+
+
+
